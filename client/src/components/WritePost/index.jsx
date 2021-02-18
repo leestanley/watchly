8b265fdfc89ebe2dc7f4749a9c061aaf4f2f5a8d
chanhenry54 <hechan@ucsd.edu>
2021-02-17 21:28:22 -0800

    finished post/writepost and created home page

--- client/src/components/WritePost/index.jsx
+++ client/src/components/WritePost/index.jsx
@@ -6,20 +6,13 @@ import './style.scss';
 const WritePost = () => {
     return (
         <div className="write-post-container">
-            <h3 className="write-post-header">Write a post</h3>
-            <input className="post-input" placeholder="I love Start Up!"></input>
-            <div className="post-options">
-                <div className="upload-pic">
-                    <PictureOutlined />
-                    <p className="post-option-text">Upload Picture</p>
-                </div>
-                <div className="submit-review">
-                    <StarOutlined />
-                    <p className="post-option-text">Submit Review</p>
-                </div>
-                <div className="share-episode">
-                    <FundOutlined />
-                    <p className="post-option-text">Share Episode</p>
+            <h2 className="write-post-header">Write a post</h2>
+            <div className="write-post-form">
+                <input className="post-input" placeholder="I love Start Up!"></input>
+                <div className="post-options">
+                    <p className="post-option-text"><PictureOutlined />Upload Picture</p>
+                    <p className="post-option-text"><StarOutlined />Submit Review</p>
+                    <p className="post-option-text"><FundOutlined />Share Episode</p>
                 </div>
             </div>
         </div>
---
+++ client/src/pages/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import Post from './components/Post';
+import WritePost from './components/WritePost';
+
+const HomePage = () => {
+    return (
+        <div className="HomePage">
+            <WritePost />
+            <Post />
+        </div>
+    );
+}
+
+export default HomePage;
