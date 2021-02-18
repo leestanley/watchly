ae10013260e8bee28ffea6fe81902420f0f53661
Jeffrey Ha <jha@ucsd.edu>
2021-02-18 01:21:56 -0800

    added title & navbar

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,11 +1,15 @@
+import Title from '../../components/Title';
+import Navbar from '../../components/Navbar';
 import Post from '../../components/Post';
 import WritePost from '../../components/WritePost';
 
 const HomePage = () => {
     return (
         <div className="HomePage">
+            <Title />
             <WritePost />
             <Post />
+            <Navbar />
         </div>
     );
 }
