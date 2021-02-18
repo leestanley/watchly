38c0d65c4ef03119cdb5852db16b0140e8d3dcba
leestanley <stanleylee91776@gmail.com>
2021-02-17 21:10:31 -0800

    react router, boilerplating folder structure & search page

---
+++ client/src/pages/SearchPage/index.jsx
@@ -0,0 +1,13 @@
+import './style.scss';
+
+import Search from '../../components/SearchCard';
+
+function SearchPage() {
+  return (
+    <div className="SearchPage">
+      <Search />
+    </div>
+  );
+}
+
+export default SearchPage;
