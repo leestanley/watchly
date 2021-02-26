1073005086fb1165cdb5fc6c05f611e733ef9762
chanhenry54 <hechan@ucsd.edu>
2021-02-25 23:20:19 -0800

    added json connections

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,11 +1,20 @@
 import Post from '../../components/Post';
 import WritePost from '../../components/WritePost';
+import postJSON from '../../assets/posts.json';
 
 const HomePage = () => {
+    const renderPosts = () => {
+        return postJSON.posts.map((post) => {
+            return (
+                <Post post={post} />
+            )
+        });
+    };
+
     return (
         <div className="HomePage">
             <WritePost />
-            <Post />
+            {renderPosts()}
         </div>
     );
 }
--- client/src/pages/SearchPage/index.jsx
+++ client/src/pages/SearchPage/index.jsx
@@ -4,15 +4,24 @@ import SearchCard from '../../components/SearchCard';
 import ShowCard from '../../components/ShowCard';
 import FriendCard from '../../components/FriendCard';
 import Post from '../../components/Post';
+import postJSON from '../../assets/posts.json';
 
 function SearchPage() {
+  const renderPosts = () => {
+    return postJSON.posts.map((post) => {
+      return (
+        <Post post={post} />
+      )
+    });
+  };
+
   return (
     <div className="SearchPage">
       <SearchCard title="Search for a TV Show or Movie" />
       <ShowCard />
       <FriendCard />
       <h2 style={{"marginTop": "2vh"}}>Global Reviews</h2>
-      <Post />
+      {renderPosts()}
     </div>
   );
 }
