f3cc655a5ce6ae2cc8ee0a8fb00cb8a08f69e5c9
Jeffrey Ha <jha@ucsd.edu>
2021-02-25 21:43:02 -0800

    backend

---
+++ server/routes/users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const f = require('../system');
+
+router.get('/createUser', async (req, res) => {
+    let username = req.query.username;
+    let name = req.query.name;
+    let email = req.query.email;
+    
+    if (username && username.length > 0)
+        if (name && name.length > 0)
+            if (email && email.length > 0)
+                res.json(await f.createUser(username, email, name));
+            else
+                res.json(`Please provide an email.`);
+        else
+            res.json(f.createError(`Please provide a name.`))
+    else
+        res.json(f.createError(`Please provide a username.`));
+});
+
+router.get('/u/:username', async (req, res) => {
+    let username = req.params.username;
+
+    if (username && username.length > 0)
+        res.json(await f.getUser(username));
+    else
+        res.json(f.createError(`Please provide a username.`));
+});
+
+router.get('/', async (req, res) => res.json(await f.getUsers(true)));
+module.exports = router;
