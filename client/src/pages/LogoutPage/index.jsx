cb62748a1b4c28e2b3d0f2c5cb04ce4f279ceecc
Jeffrey Ha <jha@ucsd.edu>
2021-03-04 01:09:28 -0800

    fixed multiple rendering notifs

--- client/src/pages/LogoutPage/index.jsx
+++ client/src/pages/LogoutPage/index.jsx
@@ -1,9 +1,29 @@
+import React, { useEffect } from 'react';
 import { notification } from 'antd';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import fbase from '../../firebase';
 
 const LogoutPage = ({ history }) => {
     const [user, loading, error] = useAuthState(fbase.auth);
+
+    useEffect(() => {
+        if (!user) return;
+        
+        fbase.logout(() => {
+            // successfully logged out!
+            notification.success({
+                message: 'Success!',
+                description: 'You are now logged out!'
+            });
+    
+            history.push('/');
+        }, (err) => {
+            notification.error({
+                message: 'Logout Error',
+                description: err.message
+            });
+        });
+    }, []);
 
     if (loading) {
         // can replace?
@@ -32,21 +52,6 @@ const LogoutPage = ({ history }) => {
         // we have to return something so we'll return an empty page.
         return <div></div>;
     }
-
-    fbase.logout(() => {
-        // successfully logged out!
-        notification.success({
-            message: 'Success!',
-            description: 'You are now logged out!'
-        });
-        
-        history.push('/');
-    }, (err) => {
-        notification.error({
-            message: 'Logout Error',
-            description: err.message
-        });
-    });
 
     return (<div>
         <p>Logging out...</p>
