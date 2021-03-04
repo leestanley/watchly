14ab70e442ba3de69bf514d178154d966db9c674
Jeffrey Ha <jha@ucsd.edu>
2021-03-04 00:19:38 -0800

    added auth protection

--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -1,13 +1,47 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import fbase from '../../firebase';
+
 import { Button } from 'antd';
 import UserSettings from '../../components/UserSettings';
+import Navbar from '../../components/Navbar';
 
 import './style.scss';
 
 import Title from '../../components/Title';
 import profilebg from '../../assets/profilebg.svg';
 
-function ProfilePage(props) {
-  return (
+function ProfilePage({ history }) {
+  const [user, loading, error] = useAuthState(fbase.auth);
+  
+  if (loading) {
+    // can replace?
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    // can replace?
+    return (
+      <div>
+        <p>
+          Error: <b>{error}</b>
+        </p>
+      </div>
+    );
+  }
+
+  if (!user) {
+    // not logged in
+    history.push('/home');
+
+    // we have to return something so we'll return an empty page.
+    return <div></div>;
+  }
+
+  return (<>
     <div className="ProfilePage">
       <Title />
       <Button>ADD SHIT HERE</Button>
@@ -17,7 +51,9 @@ function ProfilePage(props) {
       </div>
       <img src={profilebg} alt="bg" className="profilebg" />
     </div>
-  );
+
+    <Navbar />
+  </>);
 }
 
 export default ProfilePage;
