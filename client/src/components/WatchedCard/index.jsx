d644e7a6867e2ad23fe65592b78500f0e968eb87
leestanley <stanleylee91776@gmail.com>
2021-03-05 08:41:01 -0800

    we done

--- client/src/components/WatchedCard/index.jsx
+++ client/src/components/WatchedCard/index.jsx
@@ -3,7 +3,7 @@ import './style.scss';
 const WatchedCard = (props) => {
   return (
     <div className="WatchedCard">
-      <img src={props.image} alt="show" />
+      <img src={props.image} alt="show" className="paincard"/>
       <h3>{props.title}</h3>
       <p>{props.date}</p>
     </div>
